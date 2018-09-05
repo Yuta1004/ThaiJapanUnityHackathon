87b790c5e5d67d8eaba200881154e8e9034c3c49
KishidaKazuki <k.k02kamatyo@gmail>
2018-09-05 15:00:45 +0900

    [add] unityちゃんシューター完成したので追加

---
+++ Space Shooter kishidakazuki/Assets/Scripts/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start(){
+        
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null){
+            
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null){
+            
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        
+        if (other.tag == "Player"){
+            
+            gameController.AddScore(scoreValue);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Space Shooter kishidakazuki/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float yFix = 0.0f;
+
+    private Rigidbody rb;
+    private float nextFire;
+    private AudioSource audioSource;
+
+    void Start(){
+        
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update(){
+        
+        if (Input.GetButton("Fire1") && Time.time > nextFire){
+            
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audioSource.Play();
+        }
+    }
+
+    void FixedUpdate(){
+        
+        float moveHorizontal = Input.GetAxis("Horizontal"); 
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = 
+            new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+            yFix,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
