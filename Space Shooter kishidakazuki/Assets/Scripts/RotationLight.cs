330ee0af9aab9adfe4f53e2f873ee2dc79f2e26a
KishidaKazuki <k.k02kamatyo@gmail>
2018-09-05 20:56:04 +0900

    [change&fix] 光を回転＆リセット時に暗くなるバグの修正

---
+++ Space Shooter kishidakazuki/Assets/Scripts/RotationLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLight : MonoBehaviour {
+
+    public float tumble;
+
+    private float rot_x;
+    private float rot_y;
+    private float rot_z;
+
+	void Start(){
+
+        rot_x = transform.localEulerAngles.x;
+        rot_y = transform.localEulerAngles.y;
+        rot_z = transform.localEulerAngles.z;
+	}
+    void Update(){
+        rot_x += tumble;
+        this.transform.rotation = Quaternion.Euler(rot_x, rot_y, rot_z);
+    }
+}
