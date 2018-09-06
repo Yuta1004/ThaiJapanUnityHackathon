5d8d3f697c8bc3cb013d71d52814dfcb928cb984
NakagamiYuta <guguru0014@gmail.com>
2018-09-06 14:09:43 +0900

    [clean]StartButtonHandlerのリファクタリング

--- Space Shooter kishidakazuki/Assets/Scripts/StartButtonHandler.cs
+++ Space Shooter kishidakazuki/Assets/Scripts/StartButtonHandler.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StartButtonHandler : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	public void OnClick() {
 //		SceneManager.LoadScene("stage1");
 	
