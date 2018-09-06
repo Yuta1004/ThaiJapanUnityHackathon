297fe044174203edac51b8b898b534a19b490577
NakagamiYuta <guguru0014@gmail.com>
2018-09-07 08:47:14 +0900

    [add]ゴミ袋に縦揺れモーションを実装

---
+++ Space Shooter kishidakazuki/Assets/Scripts/ShakeObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class ShakeObject : MonoBehaviour {
+	public float Speed = 4.0f;
+	public float Amount = 1.5f;
+
+	void Update () {
+		float y = Mathf.Sin(Time.time * Speed) * Amount + 2;
+		Vector3 pos = gameObject.transform.position;
+		gameObject.transform.position = new Vector3 (pos.x, y, pos.z);	
+	}
+}
