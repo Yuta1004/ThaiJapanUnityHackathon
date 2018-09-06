1404b3daaff47f8eb45231b25422476f3f76d218
NakagamiYuta <guguru0014@gmail.com>
2018-09-06 16:20:42 +0900

    [add]クリックするとヘルプのポップアップが表示されるようなオブジェクトを作成

---
+++ Space Shooter kishidakazuki/Assets/Scripts/HelpPopupWindow.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPopupWindow : MonoBehaviour {
+
+	public void OnClick() {
+		gameObject.transform.GetComponent<Canvas>().enabled = false;
+	}
+}
---
+++ Space Shooter kishidakazuki/Assets/Scripts/HelpScenePopup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpScenePopup : MonoBehaviour {
+
+	public Canvas PopupCanvas;
+	public String Title;
+	[Multiline] public String Description;
+
+	void Start() {
+		PopupCanvas.enabled = false;
+	}
+
+	public void OnClick() {
+		foreach (Transform child in PopupCanvas.transform){
+			if(child.name == "PopupTitle"){
+				Text title = child.gameObject.GetComponent<Text>();
+				title.text = Title;
+			}else if (child.name == "PopupDescription") {
+				Text description = child.gameObject.GetComponent<Text>();
+				description.text = Description;
+			}
+		}
+		
+		PopupCanvas.enabled = true;
+	}
+}
