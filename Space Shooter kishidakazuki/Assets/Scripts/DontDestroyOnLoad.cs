018733174e7a8bc36effc6e3536cdb1266f92ac4
NakagamiYuta <guguru0014@gmail.com>
2018-09-06 14:09:21 +0900

    [add]ボタンに効果音を追加

---
+++ Space Shooter kishidakazuki/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	public static DontDestroyOnLoad Instance { get; private set; }
+
+	// シーン遷移してもSEを鳴らし続ける
+	void Awake (){
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad(Instance.gameObject);
+		}
+	}
+}
