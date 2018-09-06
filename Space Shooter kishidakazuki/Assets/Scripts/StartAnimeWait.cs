ed2274faee819c74b63cb7f91c15248c858f8143
NakagamiYuta <guguru0014@gmail.com>
2018-09-06 21:27:31 +0900

    [add]アニメーションを挟んだシーン遷移機能を追加

---
+++ Space Shooter kishidakazuki/Assets/Scripts/StartAnimeWait.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartAnimeWait : MonoBehaviour {
+
+	public Animation MAnimation;
+	private Boolean _endAnimation = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!MAnimation.isPlaying && !_endAnimation) {
+			FadeManager mFadeManager = FadeManager.Instance;
+			mFadeManager.LoadScene("stage1", 0.3f);
+			_endAnimation = true;
+		}
+	}
+}
